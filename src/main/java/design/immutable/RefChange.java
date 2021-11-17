5f6a06cfc2a25756265e29469bd9ccf53926779e
mawenzhen@yaeher.com <Mwz&1994>
2021-11-17 20:20:30 +0800

    feat update final class

--- src/main/java/design/immutable/RefChange.java
+++ src/main/java/design/immutable/RefChange.java
@@ -8,6 +8,7 @@ import java.lang.reflect.Field;
  * @date 2021/11/17 20:06
  */
 public class RefChange {
+    // TODO 尝试通过反射改变不可变类型，尝试失败
     public static void main(String[] args) throws Exception {
         String s = "hello world";
 
