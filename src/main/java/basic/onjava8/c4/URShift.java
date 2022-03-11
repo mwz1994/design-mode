12355f344de73c31c57d7bd067af03625b429e84
mawenzhen@yaeher.com <mawenzhen@yaeher.com>
2022-03-11 14:23:29 +0800

    URShift

---
+++ src/main/java/basic/onjava8/c4/URShift.java
@@ -0,0 +1,17 @@
+package basic.onjava8.c4;
+
+/**
+ * @author aspirin
+ * @version 1.0
+ * @date 2022/3/11 14:13
+ */
+public class URShift {
+    public static void main(String[] args) {
+        int i = 1 ;
+        System.out.println("i = " + i);
+
+        i <<= 1;
+
+        System.out.println("i<< 1 = " + i);
+    }
+}
